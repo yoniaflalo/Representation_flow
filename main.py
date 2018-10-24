b58ce82c32fe3ab06a9b1e509306859e33765cf5
Yonathan <jonathan.aflalo@alibaba-inc.com>
2018-10-24 16:47:55 +0300

    added visualisation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import torch.nn as nn
 import torch
-
+from PIL import Image
+import cv2
 
 class TVL1OF(nn.Module):
     def __init__(self, size_in, num_iter, lambda_, tau, theta, is_w_trainable=True):
@@ -31,19 +32,24 @@ class TVL1OF(nn.Module):
         self.div_y.weight.data = self.wy.unsqueeze(0).unsqueeze(0)
 
     def forward(self, x):
+        epsilon = 1e-8
         num_batch = x.shape[0]
         p1 = torch.zeros([num_batch, 2, self.size_y, self.size_x])
         p2 = torch.zeros([num_batch, 2, self.size_y, self.size_x])
         u = torch.zeros([num_batch, 2, self.size_y, self.size_x])
         v = torch.zeros([num_batch, 2, self.size_y, self.size_x])
-        rho_c = x[:, 1:, :, :] - x[:, 0:-1, :, :]
+        rho_c = (x[:, 1, :, :] - x[:, 0, :, :]).unsqueeze(1)
         grad_im = self.grad(x[:, 1:, :, :])
-        norm_grad = torch.sum(grad_im * grad_im, dim=1).unsqueeze(1)
+        norm_grad = torch.sum(grad_im * grad_im, dim=1).unsqueeze(1)+epsilon
         for i in range(self.num_iter):
             rho = rho_c + torch.sum(grad_im * u, dim=1).unsqueeze(1)
             th = self.theta * self.lambda_ * norm_grad
             v = u - (torch.abs(rho) < th).float() * rho * grad_im / norm_grad - (
                     torch.abs(rho) > th).float() * self.theta * self.lambda_ * grad_im * torch.sign(rho)
+
+            if i==self.num_iter-2:
+                tmp=1
+
             u[:, 0, :, :] = v[:, 0, :, :] + self.theta * (
                     self.div_x(p1[:, 0, :, :].unsqueeze(1))[:, :, :, 0:-1] + self.div_y(
                 p2[:, 0, :, :].unsqueeze(1))[:, :, 0:-1, :]).squeeze()
@@ -53,12 +59,46 @@ class TVL1OF(nn.Module):
             gradu1 = self.grad(u[:, 0, :, :].unsqueeze(1))
             gradu2 = self.grad(u[:, 1, :, :].unsqueeze(1))
             p1 = (p1 + self.tau / self.theta * gradu1) / (
-                    1 + self.tau / self.theta * torch.sum(torch.abs(gradu1)))
+                    1 + self.tau / self.theta * torch.sum(torch.abs(gradu1), dim=1).unsqueeze(1))
             p2 = (p2 + self.tau / self.theta * gradu2) / (
-                    1 + self.tau / self.theta * torch.sum(torch.abs(gradu2)))
+                    1 + self.tau / self.theta * torch.sum(torch.abs(gradu2), dim=1).unsqueeze(1))
         return u
 
 
-t = TVL1OF([256, 256], 20, 10.0, 0.1, 1.0)
-x = torch.randn([3, 2, 256, 256])
+
+im1 = np.array(Image.open("eval-data/Basketball/frame07.png").convert('L'))
+im2 = np.array(Image.open("eval-data/Basketball/frame09.png").convert('L'))
+x1=torch.tensor(im1)
+x2=torch.tensor(im2)
+x=torch.stack([x1,x2]).unsqueeze(0).float()
+t = TVL1OF(size_in = im1.shape, num_iter=50, lambda_=1e-1, tau=1/16, theta=0.001)
 a = t(x)
+tv=a.data.cpu().numpy()
+tv1=tv[:,0,:,:].squeeze()
+tv2=tv[:,1,:,:].squeeze()
+hsv = np.zeros([tv1.shape[0],tv1.shape[1],3])
+hsv[...,1] = 255
+mag, ang = cv2.cartToPolar(tv1, tv2)
+hsv[...,0] = ang*180/np.pi/2
+hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+hsv=hsv.astype(np.uint8)
+bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+Image.fromarray(bgr).show()
+
+#
+# im1=cv2.imread("eval-data/Basketball/frame07.png")
+# im2=cv2.imread("eval-data/Basketball/frame09.png")
+#
+# im1 = cv2.cvtColor(im1,cv2.COLOR_BGR2GRAY)
+# im2 = cv2.cvtColor(im2,cv2.COLOR_BGR2GRAY)
+#
+# hsv = np.zeros([tv1.shape[0],tv1.shape[1],3])
+# hsv[...,1] = 255
+# flow = cv2.calcOpticalFlowFarneback(im1,im2, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+# mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+# hsv[...,0] = ang*180/np.pi/2
+# hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+# hsv=hsv.astype(np.uint8)
+# bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+# Image.fromarray(bgr).show()
+#
